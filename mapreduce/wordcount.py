307ff7133438bcc1f5719efd60b6abfbfe1f4318
Jordan Engbers <jdtengbe@ucalgary.ca>
2013-11-13 16:28:56 -0700

    Changes to mapreduce presentation

--- mapreduce/wordcount.py
+++ mapreduce/wordcount.py
@@ -11,17 +11,13 @@ mr = MapReduce.MapReduce()
 # Do not modify above this line
 
 def mapper(record):
-    # key: document identifier
-    # value: document contents
-    key = record[0]
-    value = record[1]
+    key = record[0] # key: document identifier
+    value = record[1] # value: document contents
     words = value.split()
     for w in words:
       mr.emit_intermediate(w, 1)
 
 def reducer(key, list_of_values):
-    # key: word
-    # value: list of occurrence counts
     total = 0
     for v in list_of_values:
       total += v
