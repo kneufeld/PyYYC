e88b93c0dc04fe957e7f8e87fe38479e23b4164d
Jordan Engbers <jdtengbe@ucalgary.ca>
2013-11-13 15:35:39 -0700

    adjusted sentiment_analysis.sh

--- hadoop/sentiment_analysis.sh
+++ hadoop/sentiment_analysis.sh
@@ -1,5 +1,8 @@
 hadoop jar /usr/lib/hadoop/contrib/streaming/hadoop-*streaming*.jar \
--input /user/hdfs/tweets/mobileWar.json \
+-D mapred.max.split.size=67108864 \
+-D mapred.map.tasks=100 \
+-D mapred.reduce.tasks=10 \
+-input /user/hdfs/tweets/ \
 -output testOutput \
 -mapper /home/hdfs/sentiment_map.py \
 -reducer /home/hdfs/sentiment_reduce.py \
